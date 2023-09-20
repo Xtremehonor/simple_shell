255095eef638ad6810a03458ed589f6395368549
Xtremehonor <104856930+Xtremehonor@users.noreply.github.com>
2023-09-20 17:13:31 +0300

    Update parse_path.c

--- parse_path.c
+++ parse_path.c
@@ -1,46 +1,55 @@
 #include "shell.h"
+
 /**
- * _values_path - Separate the path in new strings.
- * @arg: Command input of user.
- * @env: Enviroment.
- * Return: Pointer to strings.
+ * find_executable - Searches for the executable in the PATH environment.
+ * @command: The command to search for.
+ * @env: The environment variables.
+ * Return: 0 if found, -1 if not found.
  */
-int _values_path(char **arg, char **env)
+int parse_path(char **command, char **env)
 {
-	char *token = NULL, *path_rela = NULL, *path_absol = NULL;
-	size_t value_path, len;
-	struct stat stat_lineptr;
+    char *token = NULL, *path_var = NULL, *full_path = NULL;
+    size_t command_length, path_length;
+    struct stat st;
 
-	if (stat(*arg, &stat_lineptr) == 0)
-		return (-1);
-	path_rela = _get_path(env);/** gets the content of "PATH="*/
-	if (!path_rela)
-		return (-1);
-	token = strtok(path_rela, ":"); /**tokenizes the content of "PATH="*/
-	len = _strlen(*arg); /**gets length of arg*/
-	while (token)
-	{
-		value_path = _strlen(token);
-		path_absol = malloc(sizeof(char) * (value_path + len + 2));
-		if (!path_absol)
-		{
-			free(path_rela);
-			return (-1);
-		}
-		path_absol = strcpy(path_absol, token);
-		_strcat(path_absol, "/");
-		_strcat(path_absol, *arg);
+    if (stat(*command, &st) == 0)
+        return (-1);
 
-		if (stat(path_absol, &stat_lineptr) == 0)
-		{
-			*arg = path_absol;
-			free(path_rela);
-			return (0);
-		}
-		free(path_absol);
-		token = strtok(NULL, ":");
-	}
-	token = '\0';
-	free(path_rela);
-	return (-1);
+    path_var = _get_path(env); /* Get the value of the "PATH" environment variable */
+
+    if (!path_var)
+        return (-1);
+
+    token = strtok(path_var, ":"); /* Tokenize the "PATH" value */
+    command_length = _strlen(*command); /* Get the length of the command */
+
+    while (token)
+    {
+        path_length = _strlen(token);
+        full_path = malloc(sizeof(char) * (path_length + command_length + 2));
+
+        if (!full_path)
+        {
+            free(path_var);
+            return (-1);
+        }
+
+        full_path = _strcpy(full_path, token);
+        _strcat(full_path, "/");
+        _strcat(full_path, *command);
+
+        if (stat(full_path, &st) == 0)
+        {
+            *command = full_path;
+            free(path_var);
+            return (0);
+        }
+
+        free(full_path);
+        token = strtok(NULL, ":");
+    }
+
+    token = '\0';
+    free(path_var);
+    return (-1);
 }
