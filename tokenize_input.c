408477fe271582966800d20494bdc5c9c6719c75
Xtremehonor <104856930+Xtremehonor@users.noreply.github.com>
2023-09-20 17:38:19 +0300

    Update tokenize_input.c

--- tokenize_input.c
+++ tokenize_input.c
@@ -1,46 +1,38 @@
 #include "shell.h"
-
 /**
- * tokenize_input - Tokenizes a string.
- *
- * @line_input: The input string.
- *
- * Return: A pointer to an array of pointers.
+ * tokenize - tokenizes a string
+ * @user_input: what the user inputed
+ * Return: a ptr to arr of ptrs
  */
-char **tokenize_input(char *line_input)
+
+char **tokenize(char *user_input)
 {
-	char **command_tokens = NULL;
-	char *token = NULL;
-	size_t count_tokens = 0;
+    char **command_tokens = NULL;
+    char *token = NULL;
+    size_t index = 0;
+    int size = 0;
 
-	if (line_input == NULL)
-		return (NULL);
+    if (user_input == NULL)
+        return (NULL);
 
-	size_t i = 0;
-	while (line_input[i])
-	{
-		if (line_input[i] == ' ')
-			count_tokens++;
-		i++;
-	}
+    for (index = 0; user_input[index]; index++)
+    {
+        if (user_input[index] == ' ')
+            size++;
+    }
+    if ((size + 1) == _strlen(user_input))
+        return (NULL);
+    command_tokens = malloc(sizeof(char *) * (size + 2));
+    if (command_tokens == NULL)
+        return (NULL);
 
-	if ((count_tokens + 1) == _strlen(line_input))
-		return (NULL);
+    token = strtok(user_input, " \n\t\r");
 
-	command_tokens = malloc(sizeof(char *) * (count_tokens + 2));
-	if (command_tokens == NULL)
-		return (NULL);
-
-	token = strtok(line_input, " \n\t\r");
-
-	size_t token_index = 0;
-	while (token != NULL)
-	{
-		command_tokens[token_index] = token;
-		token = strtok(NULL, " \n\t\r");
-		token_index++;
-	}
-
-	command_tokens[token_index] = NULL;
-	return (command_tokens);
+    for (index = 0; token != NULL; index++)
+    {
+        command_tokens[index] = token;
+        token = strtok(NULL, " \n\t\r");
+    }
+    command_tokens[index] = NULL;
+    return (command_tokens);
 }
