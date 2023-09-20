34fb775d6d038a720a28c6225718d1c98c4cce84
Xtremehonor <104856930+Xtremehonor@users.noreply.github.com>
2023-09-20 19:58:40 +0300

    Update get_path.c

--- get_path.c
+++ get_path.c
@@ -14,30 +14,30 @@ char *_get_path(char **env)
     /* Find the index of the PATH variable */
     while (_strncmp(env[env_index], "PATH=", path_length))
         env_index++;
-
+    
     /* If PATH is not found, return NULL */
     if (env[env_index] == NULL)
     {
         return (NULL);
     }
-
+    
     /* Calculate the length of the PATH string */
     for (path_length = 5; env[env_index][char_index]; char_index++, path_length++)
         ;
-
+    
     /* Allocate memory for the PATH string */
     path = malloc(sizeof(char) * (path_length + 1));
-
+    
     /* Check if memory allocation was successful */
     if (path == NULL)
     {
         return (NULL);
     }
-
+    
     /* Copy the PATH string to the allocated memory */
     for (char_index = 5, path_length = 0; env[env_index][char_index]; char_index++, path_length++)
         path[path_length] = env[env_index][char_index];
-
+    
     path[path_length] = '\0'; /* Add null terminator */
     return (path);
 }
