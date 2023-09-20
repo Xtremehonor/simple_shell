b00e1026fe229b643e94582fe3fe0b20e3df1b78
Xtremehonor <104856930+Xtremehonor@users.noreply.github.com>
2023-09-20 21:09:14 +0300

    Update parse_path.c

--- parse_path.c
+++ parse_path.c
@@ -7,7 +7,7 @@ size_t command_length = _strlen(*command), path_length;
 struct stat st;
 
 if (!path_var || stat(*command, &st) == 0)
-return path_var ? (free(path_var), -1) : -1;
+return (path_var ? (free(path_var), -1) : -1);
 
 token = strtok(path_var, ":");
 
@@ -17,7 +17,7 @@ path_length = _strlen(token);
 full_path = malloc(sizeof(char) * (path_length + command_length + 2));
 
 if (!full_path)
-return free(path_var), -1;
+return (free(path_var), -1);
 
 full_path = _strcpy(full_path, token);
 _strcat(full_path, "/");
@@ -27,7 +27,7 @@ if (stat(full_path, &st) == 0)
 {
 *command = full_path;
 free(path_var);
-return 0;
+return (0);
 }
 
 free(full_path);
@@ -35,5 +35,5 @@ token = strtok(NULL, ":");
 }
 
 free(path_var);
-return -1;
+return (-1);
 }
