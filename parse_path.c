2d2fbe563559041725b659e042cc4a0d4f0351f2
Xtremehonor <104856930+Xtremehonor@users.noreply.github.com>
2023-09-20 20:50:32 +0300

    Update parse_path.c

--- parse_path.c
+++ parse_path.c
@@ -11,42 +11,42 @@ int parse_path(char **command, char **env)
 char *token = NULL, *path_var = NULL, *full_path = NULL;
 size_t command_length, path_length;
 struct stat st;
+    
+    if (stat(*command, &st) == 0)
+        return (-1);
+/* Get the value of the "PATH" environment variable */
+path_var = _get_path(env); 
 
-if (stat(*command, &st) == 0)
-    return (-1);
-
-path_var = _get_path(env); /* Get the value of the "PATH" environment variable */
-
-if (!path_var)
-    return (-1);
+    if (!path_var)
+        return (-1);
 
 token = strtok(path_var, ":"); /* Tokenize the "PATH" value */
 command_length = _strlen(*command); /* Get the length of the command */
-
-while (token)
+    
+    while (token)
 {
-    path_length = _strlen(token);
+path_length = _strlen(token);
     full_path = malloc(sizeof(char) * (path_length + command_length + 2));
 
-    if (!full_path)
-    {
-        free(path_var);
-        return (-1);
-    }
+if (!full_path)
+{
+    free(path_var);
+    return (-1);
+}
 
-    full_path = _strcpy(full_path, token);
-    _strcat(full_path, "/");
-    _strcat(full_path, *command);
-
-    if (stat(full_path, &st) == 0)
-    {
-        *command = full_path;
-        free(path_var);
-        return (0);
-    }
-
-    free(full_path);
-    token = strtok(NULL, ":");
+full_path = _strcpy(full_path, token);
+_strcat(full_path, "/");
+_strcat(full_path, *command);
+    
+if (stat(full_path, &st) == 0)
+{
+*command = full_path;
+free(path_var);
+return (0);
+}
+    
+free(full_path);
+token = strtok(NULL, ":");
 }
 
 token = '\0';
