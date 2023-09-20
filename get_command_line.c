99b8bc15ef3cc42d771e69d8b5d387f5a0eb418f
Xtremehonor <104856930+Xtremehonor@users.noreply.github.com>
2023-09-20 18:29:59 +0300

    Update get_command_line.c

--- get_command_line.c
+++ get_command_line.c
@@ -7,20 +7,20 @@
  */
 char *get_command_line(void)
 {
-    /* Initialize variables */
-    char *user_input = NULL;
-    size_t input_size = 0;
+	/* Initialize variables */
+	char *lineptr = NULL;
+	size_t charter_user = 0;
 
-    /* Print shell prompt if STDIN is a terminal */
-    if (isatty(STDIN_FILENO))
-        write(STDOUT_FILENO, "$ ", 2);
+	/* Print shell prompt if STDIN is a terminal */
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "$ ", 2);
 
-    /* Read a line of input from the user */
-    if (getline(&user_input, &input_size, stdin) <= 0)
-    {
-        free(user_input); /* Clean up memory on error */
-        return (NULL);
-    }
+	/* Read a line of input from the user */
+	if (getline(&lineptr, &charter_user, stdin) <= 0)
+	{
+		free(lineptr); /* Clean up memory on error */
+		return (NULL);
+	}
 
-    return (user_input);
+	return (lineptr);
 }
